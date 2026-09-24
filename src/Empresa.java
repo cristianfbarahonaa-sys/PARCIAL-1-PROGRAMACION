f291e00999ccaec3e1866f93aa1975787e2dac49
AndresCeron08 <andresf.ceronm@uqvirtual.edu.co>
2026-09-23 22:33:18 -0500

    Se realiza en la clase empresa arreglos en donde se almacenan los datos de las demas clases, Desarrolador, ServiciosAdicionales, Proyecto, etc.

--- src/Empresa.java
+++ src/Empresa.java
@@ -1,15 +1,24 @@
+import java.util.ArrayList;
 public class Empresa {
     private String nit;
     private String nombreEmpresa;
     private String web;
     private String direccion;
     private String telefono;
+    private ArrayList<Cliente> listaClientes;
+    private ArrayList<Desarrollador> listaDesarrolladores;
+    private ArrayList<Proyecto> listaProyectos;
+    private ArrayList<ServicioAdicional> listaServiciosAdicionales;
 public Empresa (String nit, String nombreEmpresa, String web, String direccion, String telefono){
     this.nit = nit;
     this.nombreEmpresa = nombreEmpresa;
     this.web = web;
     this.direccion = direccion;
     this.telefono = telefono;
+    this.listaClientes = new ArrayList<>();
+    this.listaDesarrolladores = new ArrayList<>();
+    this.listaProyectos = new ArrayList<>();
+    this.listaServiciosAdicionales = new ArrayList<>();
     }
 
     public String getNit() {
@@ -51,4 +60,30 @@ public Empresa (String nit, String nombreEmpresa, String web, String direccion, 
     public void setTelefono(String telefono) {
         this.telefono = telefono;
     }
+    public ArrayList<Cliente> getListaClientes() {
+        return listaClientes;
+        }
+    public ArrayList<Desarrollador> getListaDesarrolladores(){
+        return listaDesarrolladores;
+    }
+    public ArrayList<Proyecto> getListaProyectos(){
+        return listaProyectos;
+    }
+    public ArrayList<ServicioAdicional> getListaServiciosAdicionales(){
+        return listaServiciosAdicionales;
+    }
+    public void agregarCliente (Cliente cliente){
+        listaClientes.add(cliente);
+    }
+    public void agregarDesarrollador (Desarrollador desarrollador){
+        listaDesarrolladores.add(desarrollador);
+    }
+    public void agregarProyecto (Proyecto proyecto){
+        listaProyectos.add(proyecto);
+    }
+    public void agregarServiciosAdicionales (ServicioAdicional servicio){
+        listaServiciosAdicionales.add(servicio);
+
+    }
 }
+
